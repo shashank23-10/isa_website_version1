f4cc057a29e4fe2f5f568766a42ad6815c82e901
Shashank  Pandey <shashankpandey2001@gmail.com>
2023-02-16 07:00:01 +0530

    before-responsiveness

--- script.js
+++ script.js
@@ -21,8 +21,25 @@ function topFunction() {
 }
 
 //Dark Mode Implementation
-(function () {
-  document.querySelector(".dark-mode").addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-  });
-})();
+function darkmode() {
+  var SetTheme = document.body;
+  SetTheme.classList.toggle("light-mode");
+  var theme;
+  if (SetTheme.classList.contains("light-mode")) {
+    console.log("Light Mode");
+    theme = "LIGHT";
+  } else {
+    console.log("Dark Mode");
+    theme = "DARK";
+  }
+
+  //Save to local storage
+  localStorage.setItem("PageTheme", JSON.stringify(theme));
+}
+
+let GetTheme = JSON.parse(localStorage.getItem("PageTheme"));
+console.log(GetTheme);
+
+if (GetTheme === "LIGHT") {
+  document.body.classList = "light-mode";
+}
